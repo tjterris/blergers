a9856a490608acc585be42501b44556bef82c27a
Brit Butler <redline6561@gmail.com>
2015-05-20 12:31:28 -0400

    Add models.

--- lib/blergers.rb
+++ lib/blergers.rb
@@ -3,9 +3,42 @@ require 'pry'
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require "blergers/version"
 require 'blergers/init_db'
+require 'blergers/importer'
 
 module Blergers
-  # Your code goes here...
+  class Post < ActiveRecord::Base
+    has_many :tags, through: :post_tags
+  end
+
+  class Tag < ActiveRecord::Base
+    has_many :posts, through: :post_tags
+  end
+
+  class PostTag < ActiveRecord::Base
+    belongs_to :post
+    belongs_to :tag
+  end
+end
+
+def add_post!(post)
+  puts "Importing post: #{post[:title]}"
+
+  tag_models = post[:tags].map do |t|
+    Blergers::Tag.find_or_create_by(name: t)
+  end
+  post[:tags] = tag_models
+
+  post_model = Blergers::Post.create(post)
+  puts "New post! #{post_model}"
+end
+
+def run!
+  blog_path = '/Users/brit/projects/improvedmeans'
+  toy = Blergers::Importer.new(blog_path)
+  toy.import
+  toy.posts.each do |post|
+    add_post!(post)
+  end
 end
 
 binding.pry
